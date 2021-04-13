e0b028c78831fade364f31a3f399129e5a80bc36
goldin5@yandex.ru <mImdem-soxved-qyngi9>
2021-04-14 00:38:37 +0400

    step 3 autoload psr 4 composer

---
+++ src/php-cli-tools.php
@@ -0,0 +1,24 @@
+<?php
+namespace Brain\Games\Cli;
+
+/**
+ * Написать строку.
+ *
+ * @param string $string Вопрос.
+ */
+function line(string $string)
+{
+    echo $string, PHP_EOL;
+}
+
+/**
+ * Задать вопрос и получить ответ.
+ * @param string $string Вопрос.
+ *
+ * @return string
+ */
+function prompt(string $string): string
+{
+    echo $string, PHP_EOL;
+    return trim(fgets(STDIN));
+}

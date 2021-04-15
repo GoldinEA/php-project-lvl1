aedc4de6dd02ac8b0724688f825f862b78219070
goldin5@yandex.ru <mImdem-soxved-qyngi9>
2021-04-15 22:38:25 +0400

    step 7

--- src/php-cli-tools.php
+++ src/php-cli-tools.php
@@ -105,3 +105,39 @@ function generateCalc(): array
             ];
     }
 }
+
+/**
+ * @param $name
+ */
+function brainGCDStart($name)
+{
+    line('Find the greatest common divisor of given numbers.');
+    $counter = 0;
+    while ($counter < 3) {
+        $GCD = generateGCD();
+        $answer = mb_strtolower(prompt('Question: ' . $GCD['QUESTION']));
+        $correctAnswer = $GCD['ANSWER'];
+        line('Your answer' . $answer);
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+            $counter++;
+        } else {
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+            line("Let's try again, $name!");
+        }
+    }
+    line("Congratulations, $name!");
+}
+
+/**
+ * @return array
+ */
+function generateGCD(): array
+{
+    $num1 = rand(1, 100);
+    $num2 = rand(1, 100);
+    return [
+        'QUESTION' => "$num1  $num2",
+        'ANSWER' => gmp_gcd($num1, $num2)
+    ];
+}
